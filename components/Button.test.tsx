57cb6053597760be941e471de3c73986dd069609
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 01:37:13 +0200

    Test Button displays children

--- components/Button.test.tsx
+++ components/Button.test.tsx
@@ -1,10 +1,10 @@
 import { fireEvent, render, screen } from "@testing-library/react-native";
 import { Button } from "./Button";
+import { Text } from "react-native";
 
 describe("Button component", () => {
   it("should render a button element", () => {
     render(<Button />);
-    screen.debug();
     const button = screen.getByRole("button");
     expect(button).toBeDefined();
   });
@@ -15,5 +15,16 @@ describe("Button component", () => {
     const button = screen.getByRole("button");
     fireEvent.press(button);
     expect(onPress).toHaveBeenCalled();
+  });
+
+  it("should display the text passed as children", () => {
+    const text = "buttonText";
+    render(
+      <Button>
+        <Text>{text}</Text>
+      </Button>
+    );
+    const button = screen.getByText(text);
+    expect(button).toBeDefined();
   });
 });
