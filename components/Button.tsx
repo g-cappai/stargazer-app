2f4fc5441e769235a0cb90f036b8c32bd7677468
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 01:37:47 +0200

    Add children to Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,15 @@
+import { ReactNode } from "react";
 import { Pressable } from "react-native";
 
 interface ButtonProps {
   onPress?: () => void;
+  children?: ReactNode;
 }
 
-export function Button({ onPress }: ButtonProps) {
-  return <Pressable role="button" onPress={onPress} />;
+export function Button({ onPress, children }: ButtonProps) {
+  return (
+    <Pressable role="button" onPress={onPress}>
+      {children}
+    </Pressable>
+  );
 }
