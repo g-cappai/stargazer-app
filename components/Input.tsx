977a04f28d8c0e1141440c38ee92dd06581f90e3
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 00:03:03 +0200

    Add alert to input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,16 @@
+import { Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 type InputProps = {
   label: string;
+  errorMessage?: string;
 };
 
-export function Input({ label }: InputProps) {
-  return <TextInput aria-label={label} />;
+export function Input({ label, errorMessage }: InputProps) {
+  return (
+    <View>
+      <TextInput aria-label={label} />
+      {errorMessage && <Text role="alert">{errorMessage}</Text>}
+    </View>
+  );
 }
