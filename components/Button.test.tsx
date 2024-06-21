a60292094d0d6ea805e580619807d5c32e015ab7
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 01:17:59 +0200

    Test button for onPress called when pressed

--- components/Button.test.tsx
+++ components/Button.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, render, screen } from "@testing-library/react-native";
 import { Button } from "./Button";
 
 describe("Button component", () => {
@@ -7,5 +7,13 @@ describe("Button component", () => {
     screen.debug();
     const button = screen.getByRole("button");
     expect(button).toBeDefined();
+  });
+
+  it("should call the onPress function when the button is pressed", () => {
+    const onPress = jest.fn();
+    render(<Button onPress={onPress} />);
+    const button = screen.getByRole("button");
+    fireEvent.press(button);
+    expect(onPress).toHaveBeenCalled();
   });
 });
