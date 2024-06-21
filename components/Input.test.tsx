df04feafb3b01d3374d173543320476800151f38
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 00:36:56 +0200

    Remove unused async to tests

--- components/Input.test.tsx
+++ components/Input.test.tsx
@@ -1,14 +1,14 @@
-import { render, screen, userEvent } from "@testing-library/react-native";
+import { render, screen, fireEvent } from "@testing-library/react-native";
 import { Input } from "./Input";
 
 describe("Input component", () => {
-  it("should render an input element", async () => {
+  it("should render an input element", () => {
     render(<Input label="inputLabel" />);
     const input = screen.getByLabelText("inputLabel");
     expect(input).toBeDefined();
   });
 
-  it("should render an helper text if there's an error", async () => {
+  it("should render an helper text if there's an error", () => {
     const { rerender } = render(<Input label="inputLabel" />);
     const helperText = screen.queryByRole("alert");
     expect(helperText).toBeNull();
@@ -18,7 +18,7 @@ describe("Input component", () => {
     expect(errorText).toHaveTextContent("error");
   });
 
-  it("should display text passed as value", async () => {
+  it("should display text passed as value", () => {
     const text = "userInput";
     render(<Input label="inputLabel" value={text} />);
     const input = screen.getByDisplayValue(text);
