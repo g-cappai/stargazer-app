45a609778a43e1a4cb6f7569ef1ec71c1302cab8
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 00:02:25 +0200

    Test for helper text on input error

--- components/Input.test.tsx
+++ components/Input.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, userEvent } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 import { Input } from "./Input";
 
 describe("Input component", () => {
@@ -6,5 +6,15 @@ describe("Input component", () => {
     render(<Input label="inputLabel" />);
     const input = screen.getByLabelText("inputLabel");
     expect(input).toBeDefined();
+  });
+
+  it("should render an helper text if there's an error", async () => {
+    const { rerender } = render(<Input label="inputLabel" />);
+    const helperText = screen.queryByRole("alert");
+    expect(helperText).toBeNull();
+
+    rerender(<Input label="inputLabel" errorMessage="error" />);
+    const errorText = screen.queryByRole("alert");
+    expect(errorText).toHaveTextContent("error");
   });
 });
