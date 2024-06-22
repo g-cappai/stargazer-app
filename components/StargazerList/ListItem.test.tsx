2671172039ff9d56130ecc31fb905844ece63a40
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-23 01:10:58 +0200

    Test for ListItem correct rendering

---
+++ components/StargazerList/ListItem.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react-native";
+import { ListItem } from "./ListItem";
+
+describe("StargazerList/ListItem", () => {
+  it("should render the list item", () => {
+    const userName = "User";
+    render(<ListItem userName={userName} avatarUrl="avatarUrl" />);
+    expect(screen.getByLabelText("User avatar")).toBeOnTheScreen();
+    expect(screen.getByText(userName)).toBeOnTheScreen();
+  });
+});
