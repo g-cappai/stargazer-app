03d490f34aa7c9083d22218624db0fedeb03633a
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-23 01:11:23 +0200

    Create ListItemComponent

---
+++ components/StargazerList/ListItem.tsx
@@ -0,0 +1,19 @@
+import { Image, Text, View } from "react-native";
+
+interface ListItemProps {
+  avatarUrl: string;
+  userName: string;
+}
+
+export function ListItem({ avatarUrl, userName }: ListItemProps) {
+  return (
+    <View>
+      <Image
+        accessibilityRole="image"
+        accessibilityLabel="User avatar"
+        source={{ uri: avatarUrl }}
+      />
+      <Text accessibilityLabel="User name">{userName}</Text>
+    </View>
+  );
+}
