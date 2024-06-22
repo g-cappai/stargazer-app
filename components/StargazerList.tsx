ed813a823057635be34de965a774344275b05959
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 21:33:30 +0200

    Show loading state when more stargazers are loading

--- components/StargazerList.tsx
+++ components/StargazerList.tsx
@@ -3,16 +3,26 @@ import { FlatList } from "react-native-gesture-handler";
 
 interface StargazerListProps {
   stargazers: { id: number; avatarUrl: string; name: string }[];
+  isLoadingMore?: boolean;
+  loadMore?: () => void;
 }
 
-export function StargazerList({ stargazers }: StargazerListProps) {
+export function StargazerList({
+  stargazers,
+  isLoadingMore,
+  loadMore,
+}: StargazerListProps) {
   return (
     <FlatList
       accessibilityRole="list"
       aria-label="Stargazers list"
       data={stargazers}
       renderItem={({ item }) => <View testID="stargazersListItem" />}
+      onEndReached={loadMore}
       ListEmptyComponent={<View accessibilityLabel="No stargazers found" />}
+      ListFooterComponent={
+        isLoadingMore ? <View accessibilityLabel="Loading more" /> : null
+      }
     />
   );
 }
