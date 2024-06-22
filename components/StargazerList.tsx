566a95cb0c69106da4061e2eeae3098989ba44c3
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-22 16:43:46 +0200

    Create StargazerList

---
+++ components/StargazerList.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+
+interface StargazerListProps {
+  stargazers: { id: number; avatarUrl: string; name: string }[];
+}
+
+export function StargazerList({ stargazers }: StargazerListProps) {
+  return (
+    <FlatList
+      accessibilityRole="list"
+      aria-label="Stargazers list"
+      data={stargazers}
+      renderItem={({ item }) => <View testID="stargazersListItem" />}
+    />
+  );
+}
