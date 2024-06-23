c4462f2ccaeffaa6084a1f2cc3fa8d6994ec0e20
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-23 19:30:35 +0200

    Show SearchForm on Homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,5 @@
-import { Text, View } from "react-native";
+import { SearchForm } from "@/components/SearchForm";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <SearchForm onSubmit={(formValues) => console.log(formValues)} />;
 }
