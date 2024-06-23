ee89b0354b5d5ad482ae6f9a1d447c54e6d74f88
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-23 17:39:33 +0200

    Create style constants

---
+++ constants/Colors.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  bgDefault: "#161b22",
+  bgDark: "#010409",
+  text: "#e6edf3",
+  textSecondary: "#8899a6",
+  border: "#30363d",
+  alert: "#ff6d6d",
+} as const;
---
+++ constants/Spacing.ts
@@ -0,0 +1,3 @@
+export const Spacing = {
+  l: 20,
+} as const;
