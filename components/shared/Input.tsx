a461214f5d6878a0163fa1654233c3da90b655ad
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-23 19:39:37 +0200

    Update Input layout

--- components/shared/Input.tsx
+++ components/shared/Input.tsx
@@ -1,18 +1,30 @@
 import { Colors } from "@/constants/Colors";
 import { Spacing } from "@/constants/Spacing";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+import { Text } from "./Text";
+import { FontSize } from "@/constants/Font";
 
 interface InputProps {
   label?: string;
   error?: boolean;
   value?: string;
+  placeholder?: string;
   onChange?: (value: string) => void;
 }
 
-export function Input({ value, onChange, label, error }: InputProps) {
+export function Input({
+  value,
+  placeholder,
+  onChange,
+  label,
+  error,
+}: InputProps) {
   return (
     <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
       <TextInput
+        placeholderTextColor={Colors.textSecondary}
+        placeholder={placeholder}
         aria-label={label}
         value={value}
         onChangeText={onChange}
@@ -30,8 +42,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: Colors.border,
     color: Colors.text,
+    fontSize: FontSize.m,
   },
   inputInvalid: {
     borderColor: Colors.alert,
+  },
+  label: {
+    fontSize: FontSize.s,
   },
 });
