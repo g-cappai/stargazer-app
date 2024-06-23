304e1ca63c2359de1a055875412811ba98c1f64d
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-23 17:40:18 +0200

    Style Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,24 @@
+import { Colors } from "@/constants/Colors";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerTitle: "Stargazer Viewers",
+        headerTintColor: Colors.text,
+        headerStyle: {
+          backgroundColor: Colors.bgDefault,
+        },
+        headerShadowVisible: false,
+        statusBarColor: Colors.bgDefault,
+        statusBarStyle: "light",
+        contentStyle: {
+          backgroundColor: Colors.bgDefault,
+        },
+        navigationBarColor: Colors.bgDark,
+      }}
+    >
       <Stack.Screen name="index" />
     </Stack>
   );
