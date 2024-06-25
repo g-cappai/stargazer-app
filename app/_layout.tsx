3930deaf341f764009147af984990a5f72e905c9
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-25 15:08:20 +0200

    Wrap main stack with QueryClientProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,29 @@
 import { Colors } from "@/constants/Colors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const queryClient = new QueryClient();
   return (
-    <Stack
-      screenOptions={{
-        headerTitle: "Stargazer Viewers",
-        headerTintColor: Colors.text,
-        headerStyle: {
-          backgroundColor: Colors.bgDefault,
-        },
-        headerShadowVisible: false,
-        statusBarColor: Colors.bgDefault,
-        statusBarStyle: "light",
-        contentStyle: {
-          backgroundColor: Colors.bgDark,
-        },
-        navigationBarColor: Colors.bgDark,
-      }}
-    >
-      <Stack.Screen name="index" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerTitle: "Stargazer Viewers",
+          headerTintColor: Colors.text,
+          headerStyle: {
+            backgroundColor: Colors.bgDefault,
+          },
+          headerShadowVisible: false,
+          statusBarColor: Colors.bgDefault,
+          statusBarStyle: "light",
+          contentStyle: {
+            backgroundColor: Colors.bgDark,
+          },
+          navigationBarColor: Colors.bgDark,
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
