82f0b7bcfeef8f43f48bcd037195ffa6474fabe7
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-25 20:36:07 +0200

    Test useStargazers

---
+++ api/useStargazers.test.tsx
@@ -0,0 +1,65 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+import { useStargazers } from "./useStargazers";
+import { renderHook } from "@testing-library/react-hooks";
+import nock from "nock";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      gcTime: Infinity,
+    },
+  },
+});
+
+const wrapper = ({ children }: PropsWithChildren) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe("useStargazers hook", () => {
+  it("should return filtered data", async () => {
+    const fakeData = [
+      { id: 1, avatar_url: "https://example.com", login: "octokit" },
+      { starred_at: "2022-01-01T00:00:00Z" },
+      {
+        starred_at: "2022-01-01T00:00:00Z",
+        user: {
+          id: 2,
+          avatar_url: "https://example.com",
+          login: "octokit",
+          events_url: "https://example.com",
+        },
+      },
+      {
+        starred_at: "2022-01-01T00:00:00Z",
+        user: null,
+      },
+    ];
+
+    nock("https://api.github.com")
+      .get("/repos/octokit/rest.js/stargazers")
+      .reply(200, fakeData);
+
+    const { result, waitFor } = renderHook(
+      () => useStargazers({ owner: "octokit", repo: "rest.js" }),
+      { wrapper }
+    );
+
+    await waitFor(() => result.current.isSuccess);
+
+    expect(result.current.data).toHaveLength(2);
+    expect(result.current.data).toEqual([
+      {
+        id: 1,
+        avatarUrl: "https://example.com",
+        name: "octokit",
+      },
+      {
+        id: 2,
+        avatarUrl: "https://example.com",
+        name: "octokit",
+      },
+    ]);
+  });
+});
