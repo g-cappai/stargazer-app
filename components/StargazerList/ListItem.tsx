d56021c4cc5890df9c49ef83cc12c6c7b7126fb0
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-06-26 21:17:30 +0200

    Style ListItem WIP

--- components/StargazerList/ListItem.tsx
+++ components/StargazerList/ListItem.tsx
@@ -1,5 +1,6 @@
-import { Image, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { Text } from "../shared/Text";
+import { Spacing } from "@/constants/Spacing";
 
 interface ListItemProps {
   avatarUrl: string;
@@ -8,13 +9,27 @@ interface ListItemProps {
 
 export function ListItem({ avatarUrl, userName }: ListItemProps) {
   return (
-    <View testID="stargazersListItem">
+    <View testID="stargazersListItem" style={styles.container}>
       <Image
         accessibilityRole="image"
         accessibilityLabel="User avatar"
         source={{ uri: avatarUrl }}
+        style={styles.avatar}
       />
       <Text accessibilityLabel="User name">{userName}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: Spacing.m,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+});
